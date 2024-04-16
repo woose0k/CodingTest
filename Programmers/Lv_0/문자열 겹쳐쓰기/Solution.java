795ccf4155d73681afaca459662583a39122dcc4
woose0k <dntjr4733@gmail.com>
2024-04-16 14:12:59 +0900

    4/16

---
+++ Programmers/Lv_0/문자열 겹쳐쓰기/Solution.java
@@ -0,0 +1,9 @@
+class Solution {
+    public String solution(String my_string, String overwrite_string, int s) {
+        String answer = my_string.substring(0, s) + overwrite_string;
+        if (my_string.length()-overwrite_string.length() >= s) {
+            answer += my_string.substring(answer.length());
+        }
+        return answer;
+    }
+}
