db3d05fd49b988e14a068c591d7686ba07a15668
woose0k <dntjr4733@gmail.com>
2024-05-10 23:04:16 +0900

    5/10

---
+++ Programmers/Lv_0/콜라츠 수열 만들기/Solution.java
@@ -0,0 +1,17 @@
+import java.util.ArrayList;
+import java.util.List;
+class Solution {
+    public int[] solution(int n) {
+         List<Integer> answer = new ArrayList<>();
+        answer.add(n);
+        while (n != 1) {
+            if (n % 2 == 0) {
+                n /= 2;
+            } else {
+                n = n * 3 + 1;
+            }
+            answer.add(n);
+        }
+        return answer.stream().mapToInt(i -> i).toArray();
+    }
+}
